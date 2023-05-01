8a0ec5b0bd6f57427a5ccfc938b932ec17ae2f07
swapnil solanki <ssinghsolanki686@gmail.com>
2023-05-01 16:20:15 +0530

    action

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: 'standard',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    quotes : ["error" , "double"],
+    indent : ["error" , 4]
+  }
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+const str = "hello"
+
+console.log(str)
+
+if (str === "hello") {
+    console.log("yes")
+}
